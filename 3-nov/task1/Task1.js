a4c1d37ca140d09da73f5f918b852a76f2eea8a3
Mohammad-Sawalha98 <115452622+Mohammad-Sawalha98@users.noreply.github.com>
2022-11-03 16:04:14 -0700

    task1

---
+++ 3-nov/task1/Task1.js
@@ -0,0 +1,27 @@
+let x= 100;
+
+if ( x < 0) {
+  document.write("Invalid entry") ;
+
+}
+else if ( x < 50) {
+    document.write ( "Fail") ;
+       } 
+else if (  x < 60) {
+        document.write ("D");
+           } 
+else if ( x < 70) {
+             document.write  ("C");
+  } 
+else if ( x < 80) {
+   document.write  ("B");
+  } 
+else if (x < 90) {
+   document.write  ("A" );
+  } 
+else if ( x <= 100) {
+    document.write  ("A+");
+  } 
+else {
+    document.write  ("invalid value") ;
+  }
---
+++ 3-nov/task2/task2.js
@@ -0,0 +1,4 @@
+const PI = 3.141592653589793;
+ var x = 5 ;
+y= 2* PI * x ; 
+document.write (y) ;
